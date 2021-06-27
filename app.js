e2bebeb226cfa31f36e2a0d6cd07215bc8ed2a6d
DennisWei9898 <denniseasycard@gmail.com>
2021-06-27 21:55:35 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express')
-const handlebars = require('express-handlebars') // 引入 handlebars
+const passport = require('./config/passport')
+const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const session = require('express-session')
 const app = express()
 const port = 3000
-
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-
+const bodyParser = require('body-parser')
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+// 設定 view engine 使用 handlebars
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user // 加這行
+  next()
+})
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
 
-require('./routes')(app)
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app, passport)
 
 module.exports = app
